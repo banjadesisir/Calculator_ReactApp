aeaee1ccb7a0771d5b022d877dcb66c5e1540545
shirshi <shishirbanjade7@gmail.com>
2024-03-06 21:39:23 +0545

    ui changes and keyboard feature

--- src/App.js
+++ src/App.js
@@ -1,44 +1,71 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const App=()=>{
-  const[result,setResult]=useState("");
-  const clickHandler=(event)=>{
-    setResult(result.concat(event.target.value))
-  }
-  
-  const clearDisplay=()=>{
-    setResult("");
+const Shishir = () => {
+  const [result, setResult] = useState("");
 
+  const clickHandler = (event) => {
+    setResult(result.concat(event.target.value));
   }
-  const calculate=()=>{
+
+  const clearDisplay = () => {
+    setResult("");
+  }
+
+  const calculate = () => {
     // eslint-disable-next-line
     setResult(eval(result).toString());
   }
-  return(
-   <div className='calc'>
-    <input type='text' placeholder='0' id='answer' value={result}/>
-    <input type='button' value={9} className='button' onClick={clickHandler}/>
-    <input type='button' value={8}className='button'onClick={clickHandler}/>
-     <input type='button' value={7}className='button'onClick={clickHandler}/>
-    <input type='button' value='.'className='button'onClick={clickHandler}/>
-    <input type='button' value={6}className='button'onClick={clickHandler}/>
-    <input type='button' value={5}className='button'onClick={clickHandler}/>
-    <input type='button' value={4}className='button'onClick={clickHandler}/>
-    <input type='button' value='-'className='button'onClick={clickHandler}/>
-    <input type='button' value={3}className='button'onClick={clickHandler}/>
-    <input type='button' value={2}className='button'onClick={clickHandler}/>
-    <input type='button' value='1'className='button'onClick={clickHandler}/>
-    <input type='button' value='+'className='button'onClick={clickHandler}/>
-    <input type='button' value='0'className='button'onClick={clickHandler}/>
-    <input type='button' value='*'className='button'onClick={clickHandler}/>
-    <input type='button' value='/'className='button'onClick={clickHandler}/>
-    <input type='button' value='%'className='button'onClick={clickHandler}/>
-    <input type='button' value='Clear'className='button button1' onClick={clearDisplay}/>
-    <input type='button' value='='className='button button1' onClick={calculate}/>
 
-   </div>
+  useEffect(() => {
+    // eslint-disable-next-line
+    const handleKeyDown = (event) => {
+      const key = event.key;
+
+      if (!isNaN(key) || key === '.') {
+        setResult(result.concat(key));
+      } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        setResult(result.concat(' ' + key + ' '));
+      } else if (key === 'Enter') {
+        calculate();
+      } else if (key === 'Backspace') {
+        setResult(result.slice(0, -1));
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [result]);
+
+  return (
+    <div className='calc'>
+      <input type='text' placeholder='0' id='answer' value={result} readOnly />
+      <div className='buttons'>
+      <input type='button' value={9} className='button' onClick={clickHandler} />
+      <input type='button' value={8} className='button' onClick={clickHandler} />
+      <input type='button' value={7} className='button' onClick={clickHandler} />
+      <input type='button' value='.' className='button' onClick={clickHandler} />
+      <input type='button' value={6} className='button' onClick={clickHandler} />
+      <input type='button' value={5} className='button' onClick={clickHandler} />
+      <input type='button' value={4} className='button' onClick={clickHandler} />
+      <input type='button' value='-' className='button' onClick={clickHandler} />
+      <input type='button' value={3} className='button' onClick={clickHandler} />
+      <input type='button' value={2} className='button' onClick={clickHandler} />
+      <input type='button' value='1' className='button' onClick={clickHandler} />
+      <input type='button' value='+' className='button' onClick={clickHandler} />
+      <input type='button' value='0' className='button' onClick={clickHandler} />
+      <input type='button' value='*' className='button' onClick={clickHandler} />
+      <input type='button' value='/' className='button' onClick={clickHandler} />
+      <input type='button' value='%' className='button' onClick={clickHandler} />
+      <input type='button' value='Clear' className='button button1' onClick={clearDisplay} />
+      <input type='button' value='=' className='button button1' onClick={calculate} />
+
+      </div>
+    </div>
   )
 }
 
-export default App;
+export default Shishir;
